2e21c4662dd7b6d4a0261a1255dd408c01d85c91
cernwang <cernwnag@tencent.com>
2015-11-29 21:26:38 +0800

    sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -3,10 +3,10 @@ use rtb5;
 
 CREATE TABLE IF NOT EXISTS `user` (
 `user_id` BIGINT( 11 ) NOT NULL,
-`user_name` VARCHAR( 1023 ) NOT NULL,
-`account` VARCHAR( 1023 ) ,
-`password` VARCHAR( 1023 ) NOT NULL,
-`token` VARCHAR( 1023 ) NOT NULL,
+`user_name` VARCHAR( 255 ) NOT NULL,
+`account` VARCHAR( 255 ) ,
+`password` VARCHAR( 255 ) NOT NULL,
+`token` VARCHAR( 255 ) NOT NULL,
 `user_ext` BLOB,
  PRIMARY KEY(`user_id`),
  UNIQUE (`token`) 
@@ -14,16 +14,16 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 CREATE TABLE IF NOT EXISTS `union` (
 `union_id` BIGINT( 11 ) NOT NULL,
-`union_name` VARCHAR( 1023 ) NOT NULL,
-`url_pattern` VARCHAR( 1023 ) NOT NULL,
+`union_name` VARCHAR( 255 ) NOT NULL,
+`url_pattern` VARCHAR( 255 ) NOT NULL,
 `union_ext` BLOB,
  PRIMARY KEY(`union_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
 
 CREATE TABLE IF NOT EXISTS `campaign` (
 `campaign_id` BIGINT( 11 ) NOT NULL,
-`campaign_name` VARCHAR( 1023 ) NOT NULL,
-`pattern` VARCHAR( 1023 ) NOT NULL,
+`campaign_name` VARCHAR( 255 ) NOT NULL,
+`pattern` VARCHAR( 255 ) NOT NULL,
 `union_id` BIGINT( 11 ) NOT NULL,
 `percent` FLOAT( 11 ) NOT NULL,
  PRIMARY KEY(`campaign_id`),
@@ -36,10 +36,10 @@ CREATE TABLE IF NOT EXISTS `click` (
 `union_id` BIGINT( 11 ) NOT NULL,
 `campaign_id` BIGINT( 11 ) NOT NULL,
 `click_time` BIGINT( 11 ) NOT NULL,
-`origin_url` VARCHAR(1023) ,
-`second_jump_url` VARCHAR(1023) ,
-`tags` VARCHAR(1023) ,
-`ip` VARCHAR(1023) ,
+`origin_url` VARCHAR(255) ,
+`second_jump_url` VARCHAR(255) ,
+`tags` VARCHAR(255) ,
+`ip` VARCHAR(255) ,
  PRIMARY KEY(`click_id`),
  FOREIGN KEY (`user_id`)  REFERENCES `user`(`user_id`),
  FOREIGN KEY (`union_id`)  REFERENCES `union`(`union_id`),
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `order` (
 `trading_volume` BIGINT( 11 ) NOT NULL,
 `order_time` BIGINT( 11 ) ,
 `valid_time` BIGINT( 11 ),
-`currency_type` VARCHAR(1023)  DEFAULT NULL,
+`currency_type` VARCHAR(255)  DEFAULT NULL,
  PRIMARY KEY(`order_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 
 
